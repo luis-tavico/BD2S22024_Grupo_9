5d378d6808da16b6473219a69bb36f4ead509785
Randall Ramos <53588320+chrissramos@users.noreply.github.com>
2024-08-16 21:16:12 -0600

    Funciones 3 y 4

--- Practica1/Funciones.sql
+++ Practica1/Funciones.sql
@@ -1,1 +1,24 @@
 USE BD2;
+
+GO
+CREATE FUNCTION F3 (
+	@UserId uniqueidentifier
+)
+RETURNS TABLE
+AS
+	
+	RETURN (SELECT * FROM practica1.Notification WHERE UserId=@UserId)
+
+;
+
+
+
+
+GO
+CREATE FUNCTION F4 ()
+RETURNS TABLE
+AS
+
+	 RETURN (SELECT * FROM practica1.HistoryLog)
+
+;
