b44beac3f5801b10d8a72c1d01bd1416dac7b4ea
Herberth3 <ibiem3@gmail.com>
2024-10-25 19:17:04 -0600

    update: implementar delete en libros

--- Proyecto2/backend/src/route/libros.router.js
+++ Proyecto2/backend/src/route/libros.router.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {createLibro,getLibros,updateLibro} from '../controllers/libros.controllers.js';
+import {createLibro,getLibros,updateLibro,deleteLibro} from '../controllers/libros.controllers.js';
 const router = express.Router();
 router.post('/libros', createLibro);
 router.get('/libros', getLibros);
 router.put('/libros/:id', updateLibro);
+router.delete('/libros/:id', deleteLibro);
 export default router;
